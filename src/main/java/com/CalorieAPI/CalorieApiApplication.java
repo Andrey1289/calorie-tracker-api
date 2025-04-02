30cab26c348459b5223bd93f77636d7341a47675
Andrey Galkovskii <dobpuha@gmail.com>
2025-04-02 21:06:29 +0700

    Initial commit: Spring Boot project structure

---
+++ src/main/java/com/CalorieAPI/CalorieApiApplication.java
@@ -0,0 +1,13 @@
+package com.CalorieAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalorieApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalorieApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CalorieAPI/CalorieApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CalorieAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalorieApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
